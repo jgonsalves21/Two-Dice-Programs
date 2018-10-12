d441dafd62ca216f626b5b6fb20a56d739b82db3
jgonsalves21 <42977061+jgonsalves21@users.noreply.github.com>
2018-10-12 13:37:34 -0400

    Add files via upload

---
+++ Doubles.java
@@ -0,0 +1,13 @@
+
+public class Doubles {
+
+	public static void main(String[] args) 
+	{
+		DiceClass dice1 = new DiceClass();
+		DiceClass dice2 = new DiceClass();
+		
+		System.out.print(dice1.roll());
+
+	}
+
+}
