5a0c4bdb05b6ca98602b1af1f9920f74ee77ed17
jgonsalves21 <42977061+jgonsalves21@users.noreply.github.com>
2018-10-12 13:26:11 -0400

    Add files via upload

---
+++ DiceClass.java
@@ -0,0 +1,47 @@
+
+public class DiceClass 
+{
+
+	private int numRoll;
+	
+	
+	
+	public void Dice()
+	{
+		
+		numRoll = 0;
+		
+	}
+	
+	
+	
+	public int roll()
+	{
+		numRoll ++;
+		return (int)(Math.random()*6)+1;
+		
+	}
+	
+	public int getRoll()
+	{
+		return numRoll;
+	}
+	
+	public void reset()
+	{
+		numRoll = 0;
+	}
+	
+	public int getNumRoll()
+	{
+		return numRoll;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
